e0c242c53c18dba7f66befd527c3f2e547ee0d69
TharushaDilmith <tharushadilmith99@gmail.com>
2024-02-03 16:34:47 +0530

    Added reducer

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const CAKE_ORDERD = 'CAKE_ORDERD';
+
+function cakeOrderd() {
+    return {
+        type: CAKE_ORDERD,
+        quantity: 1
+    };
+}
+
+const initialState = {
+    numOfCakes: 10
+};
+
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERD:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - action.quantity
+            };
+        default:
+            return state;
+    }
+};
