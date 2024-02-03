f68ecab22158a01b521ba699bb5d8b1d3b02a08e
TharushaDilmith <tharushadilmith99@gmail.com>
2024-02-03 19:32:11 +0530

    Added cake restocked case

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const CAKE_ORDERD = 'CAKE_ORDERD';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 
 function cakeOrderd() {
     return {
         type: CAKE_ORDERD,
         quantity: 1
+    };
+}
+
+function cakeRestocked(quantity = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        quantity: quantity
     };
 }
 
@@ -19,7 +30,25 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes - action.quantity
             };
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.quantity
+            };
         default:
             return state;
     }
 };
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+
+store.dispatch(cakeOrderd());
+store.dispatch(cakeOrderd());
+store.dispatch(cakeOrderd());
+store.dispatch(cakeRestocked(5));
+store.dispatch(cakeOrderd());
+
+unsubscribe();
